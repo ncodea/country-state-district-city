0aae486a705fb26e3b5788b080496c9fe88fefa6
Rahmathulla <rahmathullatm@gmail.com>
2019-09-06 20:32:18 +0530

    Added cities and districts template

---
+++ sql/cities.sql
@@ -0,0 +1,3 @@
+INSERT INTO cities (id, name, district_id) VALUES
+(191001000, 'Alappuzha', 19100),
+(191001001, 'Ambalapuzha', 19100)
---
+++ sql/districts.sql
@@ -0,0 +1,3 @@
+INSERT INTO districts (id, name, state_id) VALUES
+(19100, 'Alappuzha', 19),
+(19101, 'Ernakulam', 19)
